c2872adebbb5476deedf7e99d9913a1d81785c43
Tomohiro Tashiro <tashiro@wni.com>
2021-05-05 13:45:05 +0900

    Initial Commit

---
+++ GSIMap2/GSIMap2App.swift
@@ -0,0 +1,17 @@
+//
+//  GSIMap2App.swift
+//  GSIMap2
+//
+//  Created by tasshy on 2021/05/05.
+//
+
+import SwiftUI
+
+@main
+struct GSIMap2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
